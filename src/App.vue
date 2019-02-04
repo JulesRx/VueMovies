3129ed20b00ca023d99c26beb5aa526867c0eea8
JulesRx <25800503+JulesRx@users.noreply.github.com>
2019-02-04 13:24:56 +0100

    Axios

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
@@ -9,5 +9,18 @@
 </template>
 
 <style lang="scss">
-  @import '@/scss/base.scss';
+@import "@/scss/base.scss";
 </style>
+
+<script>
+export default {
+  mounted() {
+    this.$http
+      .get("https://jsonplaceholder.typicode.com/albums")
+      .then(function(data) {
+        let help = data.data;
+        console.log(help);
+      });
+  }
+};
+</script>
