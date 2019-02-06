6d85a46326859cd86daea581ded6a89168f21641
JulesRx <25800503+JulesRx@users.noreply.github.com>
2019-02-06 22:05:43 +0100

    NotFound

--- src/router.js
+++ src/router.js
@@ -3,6 +3,9 @@ import Router from 'vue-router'
 
 import Home from '@/vue/components/home.vue'
 import About from '@/vue/components/about.vue'
+import NotFound from '@/vue/components/errors/not-found.vue'
+
+// movies
 import MovieAdd from '@/vue/components/movie/movie-add.vue'
 import MovieDetails from '@/vue/components/movie/movie-details.vue'
 import MovieEdit from '@/vue/components/movie/movie-edit.vue'
@@ -13,10 +16,11 @@ export default new Router({
   // history mode do not work with gh-pages without a custom domain: https://bit.ly/2WJawOQ
   mode: process.env.NODE_ENV === 'production' ? 'hash' : 'history',
   routes: [
-    { path: '/', name: 'Home', component: Home },
     { path: '/movie/add', name: 'MovieCreate', component: MovieAdd },
     { path: '/movie/:id', name: 'MovieDetails', component: MovieDetails },
     { path: '/movie/:id/edit', name: 'MovieEdit', component: MovieEdit },
     { path: '/about', name: 'About', component: About },
+    { path: '/', name: 'Home', component: Home },
+    { path: '/404', alias: '*', component: NotFound }
   ]
 })
---
+++ src/vue/components/errors/not-found.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="pnot-found">
+    <h1>Page not found</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.pnot-found {
+  text-align: center;
+}
+</style>
