311678bf27b5477c0d7de593a48397621be0a0e9
JulesRx <25800503+JulesRx@users.noreply.github.com>
2019-02-10 11:22:08 +0100

    Webpack watch

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const WebpackShellPlugin = require('webpack-shell-plugin');
 
 const config = {
   entry: './src/vue/main.js',
@@ -39,7 +40,8 @@ const config = {
   },
   plugins: [
     new VueLoaderPlugin(),
-    new CopyWebpackPlugin(['src/static',], {})
+    new CopyWebpackPlugin(['src/static',], {}),
+    new WebpackShellPlugin({ onBuildEnd: ['node src/node/app.js'] })
   ]
 }
 
