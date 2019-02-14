07ab73579811b070cc6d71bd3ae4fa9d0c738414
JulesRx <25800503+JulesRx@users.noreply.github.com>
2019-02-14 13:55:55 +0100

    Webpack comments

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const WebpackShellPlugin = require('webpack-shell-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 const config = {
   entry: './src/vue/main.js',
@@ -52,15 +52,21 @@ module.exports = (env, argv) => {
   if (argv.mode === 'development') {
     config.plugins.push(
       new WebpackShellPlugin({ onBuildEnd: ['node src/node/app.js'] })
-    )
+    );
   }
 
   if (argv.mode === 'production') {
-    // config.optimization = {
-    //   minimizer: [
-    //     new UglifyJsPlugin()
-    //   ]
-    // }
+    // ES6 issue
+    // config.plugins.push(
+    //   new UglifyJsPlugin({
+    //     uglifyOptions: {
+    //       compress: {
+    //         warnings: false
+    //       },
+    //       sourceMap: true
+    //     }
+    //   })
+    // );
   }
 
   return config;
